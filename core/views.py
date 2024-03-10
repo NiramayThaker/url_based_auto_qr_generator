d97dd6bca9834e7a5eb12942272adfc298691847
Admin <thakerniramay@gmail.com>
2024-03-10 21:47:30 +0530

    Getting user data input and generating QR from it

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, redirect 
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserInfoForm
 
 def home(request):
-    return render(request, "form.html")
+    form = UserInfoForm()
+
+    if request.method == "POST":
+        form = UserInfoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, "form.html", context=context)
