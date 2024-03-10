e23b1fd0d5b57cc5274b07fd9e8db9e63269b42d
Admin <thakerniramay@gmail.com>
2024-03-11 00:56:56 +0530

    Creating landing page for redirected url from QR

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('user/<str:pk>', views.user_info, name="home"),
+    path('user/<str:pk>', views.user_info, name="user-info"),
+    path('info', views.info, name="info"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import UserInfo
+from django.contrib.auth.models import User
 from .forms import UserInfoForm
 import qrcode
 
 
 def home(request):
     form = UserInfoForm()
-    id = UserInfo.objects.values_list('user_id', flat=True)[1]
-    print(f"\n\n\n\n {id} \n\n\n\n")
+    # id = UserInfo.objects.values_list('user_id', flat=True)[0]
+    # print(f"\n\n\n\n {id} \n\n\n\n")
+
+    user_name = User.objects.get(id=request.user.id)
+    print(f"\n\n\n\n\n\n\n {user_name} \n\n\n\n\n\n\n")
+    # uuid = UserInfo.objects.get(name=user_name)
+    # print(f"\n\n\n\n\n\n\n {uuid.user_id} \n\n\n\n\n\n\n")
 
     if request.method == "POST":
         form = UserInfoForm(request.POST)
         if form.is_valid():
             form.save()
-            generate_qr(id, request.user)
+            # generate_qr(id, request.user)
 
             return redirect('home')
 
@@ -27,7 +33,13 @@ def generate_qr(id, username):
 
 
 def user_info(request, pk):
-    user_info = UserInfo.objects.get(id=pk)
+    user_info = UserInfo.objects.get(user_id=pk)
+
+    context = {'user_info': user_info}
+    return render(request, 'user.html', context=context)
+
+def info(request):
+    user_info = UserInfo.objects.get(id=1)
 
     context = {'user_info': user_info}
     return render(request, 'user.html', context=context)
